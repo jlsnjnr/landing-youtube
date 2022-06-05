1b47437748aa39c38790460f20c00168fa721e8a
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-06-05 14:44:14 -0300

    mobile first and readme

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">
+  Landing Page
+</h1>
+
+<p align="center">
+  <img alt="" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- React Js (Vite)
+- [Tailwind CSS](https://tailwindcss.com/)
+- React Icons
+
+## Getting Started
+### Install dependencies:
+
+```bash
+npm install
+```
+
+### Run development server
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Commands
+
+- `npm run dev`: run development server
+
+## 🚧 Projeto:
+
+Em conclusão
+
+## 🎨 Inspiração:
+
+Figma: https://www.figma.com/file/RWyjCBDnipk2xcQsMuj1fG/Landing-Page-(Creative-Agency)-(Community)?node-id=1%3A68
+
+Feito com ♥ by Joelson Junior
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,20 +1,27 @@
 import BannerMobile from "./../assets/banner-mobile.png";
+import blue from "./../assets/blue.svg";
+import orange from "./../assets/orange.svg";
 
 export function Banner() {
   return (
-    <main className="pt-20 text-center">
-      <h1 className="text-primary font-[900] text-2xl">
+    <main className="pt-20 text-center relative">
+      <h1 className="text-primary font-[900] text-[28px]">
         Make your dream business goal come true
       </h1>
       <p className="text-secondary text-sm py-6 font-[300]">
         when you need us for improve your business, then come with us to help
-        your business have reach it, you just sit and feel that goal.{" "}
+        your business have reach it, you just sit and feel that goal.
       </p>
       <button className="w-36 h-9 bg-blue-btn rounded-3xl text-white drop-shadow-3xl text-xs">
         Start Project
       </button>
 
       <img src={BannerMobile} className="py-4 mx-auto" alt="Banner Mobile" />
+      
+      <img src={blue} className="absolute bottom-[270px] left-0" alt="svg icon" />
+
+      <img src={orange} className="absolute top-[60px] right-0" alt="svg icon" />
+
     </main>
   );
 }
